8d84dd765217b1342df30d4da59f2a12296e7782
Lam Chan <lamchakchan@gmail.com>
2021-09-15 10:28:01 -0500

    added force process exit for async loop

--- subscriber.js
+++ subscriber.js
@@ -4,6 +4,8 @@ const Amqp = require('amqplib');
     const connection = await Amqp.connect(`amqps://${process.env.RMQ_USER}:${process.env.RMQ_PASSWORD}@${process.env.RMQ_HOSTNAME}/${process.env.RMQ_USER}`);
     const channel = await connection.createConfirmChannel();
 
+    setTimeout(process.exit, 1500);
+
     await channel.consume('queue1', (msg) => {
         console.log(msg);
         channel.ack(msg);
